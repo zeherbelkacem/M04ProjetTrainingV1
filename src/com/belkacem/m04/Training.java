b75f46ef38d82c5e637d830e6c4370b5fd9242b9
Stagiaires11 <Stagiaires11P@ad.fms-ea.com>
2022-03-18 13:35:24 +0100

    question 4 fixed

--- src/com/belkacem/m04/Training.java
+++ src/com/belkacem/m04/Training.java
@@ -39,6 +39,77 @@ public class Training {
 	/**
 	 * 
 	 */
+	private static void bucketSummary() {
+
+		System.out.print(ConsoleColors.RED_BOLD);
+		for (int i = 0; i < 112; i++)
+			System.out.print("-");
+		System.out.println();
+		System.out.print(
+				"    ---------------------------------------  Your bucket summary  -----------------------------------------");
+		System.out.println();
+		for (int i = 0; i < 112; i++)
+			System.out.print("-");
+		System.out.println(ConsoleColors.RESET);
+		System.out.print(ConsoleColors.GREEN_BOLD);
+		System.out.print(String.format("|%-18s|", "COURSE"));
+		System.out.print(String.format("%-10s|", "DAYS/NB"));
+		System.out.print(String.format("%-41s|", "DESCRIPTION"));
+		System.out.print(String.format("%-10s|", "QUANTITY)"));
+		System.out.print(String.format("%-12s|", "UNIT PRICE"));
+		System.out.print(String.format("%-15s|", "TOTAL PRICE"));
+		System.out.println();
+		for (int i = 0; i < 112; i++)
+			System.out.print("-");
+		System.out.println(ConsoleColors.RESET);
+		Double productTotalPrice = 0.0;
+		Double TotalPrice = 0.0;
+		for (Map.Entry<Integer, List<String>> entry : bucket.entrySet()) {
+			productTotalPrice = Double.parseDouble(entry.getValue().get(3))
+					* Double.parseDouble(entry.getValue().get(4));
+			System.out.print(String.format("|%-18s|", entry.getValue().get(0)));
+			System.out.print(String.format("%-10s|", entry.getValue().get(1)));
+			System.out.print(String.format("%-41s|", entry.getValue().get(2)));
+			System.out.print(String.format("%-10s|", entry.getValue().get(4)));
+			System.out.print(String.format("%-12s|", entry.getValue().get(3)));
+			System.out.println(String.format("%-15s|", productTotalPrice + " €"));
+
+			for (int i = 0; i < 112; i++)
+				System.out.print("-");
+			System.out.println();
+			TotalPrice += productTotalPrice;
+		}
+		System.out.print(ConsoleColors.RED_BOLD);
+		System.out.print(String.format("|%-95s|",
+				"Totals => => => To validate order (1) , (2) to cancel order or (2) to return to the menu"));
+		System.out.println(String.format("%-15s|", TotalPrice + " €"));
+		for (int i = 0; i < 112; i++)
+			System.out.print("-");
+		System.out.println(ConsoleColors.RESET);
+
+		if (bucket.isEmpty()) {
+			System.out.println("Your bucket si empty");
+
+		} else {
+			int answer = 0;
+			while (answer != 1 && answer != 2 && answer != 3) {
+				System.out.println("What do you want to do: ?(1/2,3)");
+				while (!scanner.hasNextInt()) {
+					scanner.next();
+					System.out.println("Sorry, your entry must be integer");
+				}
+				answer = scanner.nextInt();
+			}
+			if (answer == 1 || answer == 2) {
+				bucket.clear();
+				System.out.println("Your bucket is cleared, thank you.");
+			}
+		}
+	}
+
+	/**
+	 * 
+	 */
 	private static void insertDataInTrainingMap() {
 		String[] training1Array = { "JAVA", "20", "Java SE 8: sysntax, oop", "100" };
 		String[] training2Array = { "ADVANCED JAVA", "20", "Exceptions, files, thread, jcdb...", "100" };
@@ -50,7 +121,6 @@ public class Training {
 		trainings.put(3, Arrays.asList(training3Array));
 		trainings.put(4, Arrays.asList(training4Array));
 		trainings.put(5, Arrays.asList(training5Array));
-
 	}
 
 	/**
@@ -147,7 +217,7 @@ public class Training {
 			System.out.print(ConsoleColors.RESET);
 			System.out.println("To view all the available trainings,        enter (1)");
 			System.out.println("To add training to bucket,                  enter (2)");
-			System.out.println("To show bucket,                             enter (3)");
+			System.out.println("To show and validate your bucket,           enter (3)");
 			System.out.println("To remove product from bucket,              enter (4)");
 			System.out.println("To exit,                                    enter (0)");
 			System.out.println("-----------------------------------------------------");
@@ -165,6 +235,7 @@ public class Training {
 				addTrainingToBucket();
 				break;
 			case 3:
+				bucketSummary();
 				break;
 			case 4:
 
@@ -176,7 +247,6 @@ public class Training {
 			default:
 				System.out.println("Wrong entry: ONLY INTEGERS ENTRIES ( 0 to 5)");
 			}
-
 		}
 	}
 
@@ -193,15 +263,17 @@ public class Training {
 				Integer productId = 0;
 				Integer quantity = 0;
 				showBucket(bucket);
-				
+
 				/** Check if product entry is correct (available) */
 				while (!bucket.containsKey(productId)) {
-					System.out.println("Select the product number you want to remove. Entry must be integer and correct ID !");
+					System.out.println(
+							"Select the product number you want to remove. Entry must be integer and correct ID !");
 					while (!scanner.hasNextInt()) {
 						scanner.next();
 						System.out.println("Wrong entry, enter only integers:");
 					}
-					//System.out.println("Sorry the selected product not available, select the right number:");
+					// System.out.println("Sorry the selected product not available, select the
+					// right number:");
 					/** Check if choice entry is correct (integer) */
 					productId = scanner.nextInt();
 				}
@@ -229,17 +301,14 @@ public class Training {
 						bucket.get(productId).add(newQuantity.toString());
 					}
 					if (bucket.isEmpty()) {
-						System.out.println("Your bucket is empty, good bye");
-						answer = "no";//get out
-					}else {
+						answer = "no";// get out
+					} else {
 						showBucket(bucket);
 						System.out.println("Do you want to remove another Product: ?(yes/no)");
 						answer = yesOrNoChoice();
 					}
-					
-				} 
+				}
 			}
-
 		}
 	}
 
@@ -261,40 +330,70 @@ public class Training {
 	 * 
 	 */
 	private static void addTrainingToBucket() {
-		Integer quantity = 0;
-		Integer choice = 0;
-		List<String> bucketList = new ArrayList<String>();
-		displayAllTrainingListV2();
 
-		while (!trainings.containsKey(choice)) {
-			System.out.println("Enter the  training's number please (intger and!");
+		String answer = "yes";
+		while (!answer.equalsIgnoreCase("no")) {
+			Integer quantity = 0;
+			Integer choice = 0;
+			List<String> bucketList = new ArrayList<String>();
+			displayAllTrainingListV2();
+
+			while (!trainings.containsKey(choice)) {
+				System.out.println("Enter the  training's number please (intger only)");
+				while (!scanner.hasNextInt()) {
+					scanner.next();
+					System.out.println("Wrong entry, enter only integers:");
+				}
+				choice = scanner.nextInt();
+			}
+
+			/** Check if choice entry is correct (available) */
+			/***/
+			System.out.println("choose the quantity for this training: ? 1,2,3,..");
 			while (!scanner.hasNextInt()) {
 				scanner.next();
 				System.out.println("Wrong entry, enter only integers:");
 			}
-			choice = scanner.nextInt();
-		}
+			quantity = scanner.nextInt();
 
-		/** Check if choice entry is correct (available) */
-		/***/
-		System.out.println("choose the quantity for this training: ? 1,2,3,..");
-		while (!scanner.hasNextInt()) {
-			scanner.next();
-			System.out.println("Wrong entry, enter only integers:");
-		}
-		quantity = scanner.nextInt();
+			// build list for map
+			bucketList.addAll(trainings.get(choice));// copy the training list to bucket list (train
+			if (bucket.containsKey(choice)) {// add the same product -> quantity incremented
+				Integer newQuantity = quantity + Integer.parseInt(bucket.get(choice).get(4)); //
+				bucketList.add(newQuantity.toString());
+				bucket.put(choice, bucketList);
+			} else { // new product in the bucket (first time)
+				bucketList.add(quantity.toString());
+				bucket.put(choice, bucketList);
+			}
+			showBucket(bucket);
+			// after each add
+			int switchChoice = 0;
+			while (switchChoice != 4) {
+				System.out.println(
+						"Do your choice: (1) -> add new product, (2) -> validate bucket, (3) -> Ajust bucket, (4) -> exit");
+				while (!scanner.hasNextInt())
+					scanner.next();
+				switchChoice = scanner.nextInt();
+				switch (switchChoice) {
+				case 1:
+					switchChoice = 3;
+					break;
+				case 2:
+					bucketSummary();
+					break;
+				case 3:
+					removeTrainingFromBucket();
+					break;
+				case 4:
+					answer = "no";
+					break;
 
-		// build list for map
-		bucketList.addAll(trainings.get(choice));// copy the training list to bucket list (train
-		if (bucket.containsKey(choice)) {// add the same product -> quantity incremented
-			Integer newQuantity = quantity + Integer.parseInt(bucket.get(choice).get(4)); //
-			bucketList.add(newQuantity.toString());
-			bucket.put(choice, bucketList);
-		} else { // new product in the bucket (first time)
-			bucketList.add(quantity.toString());
-			bucket.put(choice, bucketList);
+				default:
+					break;
+				}
+			}
 		}
-		showBucket(bucket);
 
 	}
 
@@ -302,9 +401,7 @@ public class Training {
 	 * 
 	 * @param bucket2
 	 */
-
 	private static void showBucket(Map<Integer, List<String>> bucket2) {
-
 		for (Entry<Integer, List<String>> entry : bucket2.entrySet()) {
 			System.out.print(ConsoleColors.BLUE_BOLD);
 			System.out.print("Product N°: " + entry.getKey() + " -> ");
@@ -314,7 +411,6 @@ public class Training {
 					+ entry.getValue().get(2) + ", " + entry.getValue().get(3)));
 			System.out.print(String.format("[%-15s]", " Qunatity -> (" + entry.getValue().get(4) + ")"));
 			System.out.println(ConsoleColors.RESET);
-
 		}
 	}
 }
